dcbce52fda5fca2c1d3b4c2cb3bde79d0663879f
MaxDzioban <149308245+MaxDzioban@users.noreply.github.com>
2025-05-08 21:14:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from visualization import MainMenu
 
 
 app = QApplication(sys.argv)
-api_key = "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"
+api_key = "OPEN_AI_api_key"
 main_menu = MainMenu(api_key)
 main_menu.show()
 sys.exit(app.exec_())
